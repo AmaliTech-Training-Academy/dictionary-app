f5d25075c5a0a869a2eab26764e014e15deb8962
augustine-ok <augustine.opoku-kesse@gmail.com>
2023-04-11 14:51:44 +0000

    feature: Created search input and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import './App.scss';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
-      dictionary app
+      <Search />
     </div>
   );
 }
