a15e16123a9c59c20f0ea8d37f146f1eb7aa81b7
augustine-ok <augustine.opoku-kesse@gmail.com>
2023-04-12 12:03:16 +0000

    Feature: Created the play button and added scss styling

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import '../assets/styles/AudioPlayer.scss';
 
-const AudioPlayer: React.FC = () => {
+interface AudioPlayerProps {
+    audioUrl: string;
+  }
+
+const AudioPlayer: React.FC<AudioPlayerProps> = ({ audioUrl}) => {
+
+    
     return (
-        <div>
-          Audio  
-        </div>
+        <button className='audioPlayButton'>
+          <div className="play-icon"> 
+            <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 75 75">
+              <g fill="#A445ED" fill-rule="evenodd">
+                <circle cx="37.5" cy="37.5" r="37.5" opacity=".25"/>
+                <path d="M29 27v21l21-10.5z"/>
+              </g>
+            </svg>
+          </div>
+        </button>
     )
 };
 
