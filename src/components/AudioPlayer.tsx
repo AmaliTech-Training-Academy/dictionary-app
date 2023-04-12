69e42bec0c1a4312512b741dec325025686127d2
augustine-ok <augustine.opoku-kesse@gmail.com>
2023-04-12 11:55:58 +0000

    Created the react typescript function template for the audio player component as well as its styling file

---
+++ src/components/AudioPlayer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/AudioPlayer.scss';
+
+const AudioPlayer: React.FC = () => {
+    return (
+        <div>
+          Audio  
+        </div>
+    )
+};
+
+export default AudioPlayer;
