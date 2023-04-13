7170ff3f4ce8c2c44d28cf1634023b0b991b6c4e
john teye <john.teye@amalitech.org>
2023-04-13 21:06:57 +0000

    Updated API response component with AudioPlayer and WordNotound component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import './App.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
 import { fetchWord } from './wordAPI';
 import type { DictionaryData } from './wordAPI';
 import { Result } from './components/Result';
-
-
+import WordNotFound from './components/validation message/WordNotFound';
 const App: React.FC = () => {
 
   const [searchResult, setSearchResult] = useState<DictionaryData[]>([]);
+  useEffect(
+    () => {
+      if(searchResult.length === 0){
+        handleSearch('keyboard');
+      }
+    }
+  )
 
   const handleSearch = async (word: string) => {
     const data = await fetchWord(word);
@@ -22,7 +28,11 @@ const App: React.FC = () => {
     <div className="App">
       <Header />
       <Search onSubmit={handleSearch}/>
-      <Result result={searchResult}/>
+      {!Array.isArray(searchResult) ? (
+        <WordNotFound />
+      ) : (
+        <Result result={searchResult}/>
+      )}
     </div>
   );
 }
